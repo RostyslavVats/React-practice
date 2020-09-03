131e16849918a32478dc42d52f637f446134ff00
Rostyslav <rostikvac12@gmail.com>
2020-09-03 12:38:10 +0300

    add  style css

--- src/components/search-panel.js
+++ src/components/search-panel.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react';
 
-const SearchPanel = ()=>{
-    const searchText = 'Type here to search';
-    const searchStyle = {
-        fontSize: '20px'
-    }
-    return<input
-        style={searchStyle}
-        placeholder={searchText}
-    />;
-}
+import './search-panel.css';
+
+const SearchPanel = () => {
+    return (
+        <input type="text"
+               className="form-control search-input"
+               placeholder="type to search" />
+    );
+};
+
 export default SearchPanel;
